2bf4e3bdb949e17bf9f037a24df627bf9419231b
varshamarni <53220142+varshamarni@users.noreply.github.com>
2024-11-27 02:14:34 +0000

    lab 5 submission

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const Products = require('./products')
+const Orders = require('./orders')
 const autoCatch = require('./lib/auto-catch')
 
 /**
@@ -48,31 +49,47 @@ async function getProduct(req, res, next) {
  * Create a product
  * @param {object} req 
  * @param {object} res 
+ * @param{NextFunction} next
  */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+async function createProduct(req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
-/**
- * Edit a product
- * @param {object} req
- * @param {object} res
- * @param {function} next
- */
-async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
-}
 
-/**
- * Delete a product
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
- */
-async function deleteProduct(req, res, next) {
-  res.json({ success: true })
+async function editProduct (req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+  res.json(product)
+}
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
+}
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(order)
+}
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
+}
+async function editOrder (req, res, next) {
+  const change = req.body
+  const order = await Orders.edit(req.params.id, change)
+  res.json(order) 
+}
+async function deleteOrder (req, res, next) {
+  const response = await Orders.destroy(req.params.id)
+  res.json(response)
 }
 
 module.exports = autoCatch({
@@ -81,5 +98,8 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
+  deleteProduct,
+  listOrders,
+  createOrder,
+  deleteOrder
 });
